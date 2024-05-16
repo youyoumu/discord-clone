b0e2a10680a8243c19d0e4938ce9b5c9365c328a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-16 23:57:03 +0700

    Save access token to localstorage

--- nextjs/app/oauth/callback/discord-clone/page.tsx
+++ nextjs/app/oauth/callback/discord-clone/page.tsx
@@ -2,11 +2,20 @@
 
 import { useSearchParams } from 'next/navigation'
 import { getAccessToken } from './action'
+import { useEffect } from 'react'
 
 export default function DiscordClone() {
   const params = useSearchParams()
   const code = params.get('code') || ''
   const accessToken = params.get('access_token') || ''
+
+  useEffect(() => {
+    if (accessToken) {
+      localStorage.setItem('accessToken', accessToken)
+      console.log(localStorage.getItem('accessToken'))
+    }
+  }, [accessToken])
+
   return (
     <form action={getAccessToken}>
       <input name="code" type="text" value={code as string} readOnly />
