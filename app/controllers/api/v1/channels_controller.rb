8b6688ad8708e570e0fe55135e182214aa8ce13a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-17 14:16:52 +0700

    Create api to create channel

---
+++ app/controllers/api/v1/channels_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ChannelsController < ApiController
+  def index
+    render json: Server.find(params[:server_id]).channels
+  end
+
+  def show
+    render json: Channel.find(params[:id])
+  end
+
+  def create
+    user = current_resource_owner
+    unless user.servers.exists?(id: params[:server_id])
+      render json: {error: "You are not server owner."}
+    end
+    server = Server.find(params[:server_id])
+    channel = server.channels.build(channel_params)
+    if channel.save
+      render json: channel
+    else
+      render json: channel.errors
+    end
+  end
+
+  def update
+    user = current_resource_owner
+    unless user.servers.exists?(id: params[:server_id])
+      render json: {error: "You are not server owner."}
+    end
+    channel = Channel.find(params[:id])
+    if channel.update(channel_params)
+      render json: channel
+    else
+      render json: channel.errors
+    end
+  end
+
+  def destroy
+    channel = Channel.find(params[:id])
+    channel.destroy
+    render json: channel
+  end
+
+  private
+
+  def channel_params
+    params.require(:channel).permit(:name)
+  end
+end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,4 @@
 class Server < ApplicationRecord
   belongs_to :user
+  has_many :channels, dependent: :destroy
 end
