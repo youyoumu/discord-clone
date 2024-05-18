133c59a84d944e3d56cd82982683c5a2f12a18af
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-18 22:46:21 +0700

    Display pfp

--- nextjs/components/message.tsx
+++ nextjs/components/message.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export function Message({
   content,
   username
@@ -6,9 +8,18 @@ export function Message({
   username: string
 }) {
   return (
-    <div>
-      <div className="font-medium">{username}</div>
-      <p className="text-sm">{content}</p>
+    <div className="flex gap-2">
+      <Image
+        src="/user.png"
+        alt=""
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <div>
+        <div className="font-medium">{username}</div>
+        <p className="text-sm">{content}</p>
+      </div>
     </div>
   )
 }
