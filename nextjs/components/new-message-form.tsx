8277fa532d151685448b75b58a7a8f67ab58830c
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-18 16:47:36 +0700

    Reset form after submit

--- nextjs/components/new-message-form.tsx
+++ nextjs/components/new-message-form.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { createMessage } from '@/lib/actions'
+import { useState } from 'react'
 
 export function NewMessageForm({
   serverId,
@@ -9,12 +12,18 @@ export function NewMessageForm({
   serverId: string
   channelId: string
 }) {
+  const [content, setContent] = useState('')
+
   return (
-    <form action={createMessage}>
+    <form action={createMessage} onSubmit={() => setContent('')}>
       <div className="flex gap-2 p-2">
-        <input type="text" name="serverId" value={serverId} hidden />
-        <input type="text" name="channelId" value={channelId} hidden />
-        <Input name="content" />
+        <input type="text" name="serverId" value={serverId} hidden readOnly />
+        <input type="text" name="channelId" value={channelId} hidden readOnly />
+        <Input
+          name="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
         <Button type="submit">Submit</Button>
       </div>
     </form>
