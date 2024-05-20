97078a1d7a3b7fa4d6cbcddb16542438e00982f7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-20 20:25:00 +0700

    Format message time

--- nextjs/components/datetime.tsx
+++ nextjs/components/datetime.tsx
@@ -1,10 +1,26 @@
+'use client'
+
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
 export function Datetime({ datetime }: { datetime: string }) {
+  dayjs.extend(relativeTime)
+  const d = dayjs(datetime)
+  const now = dayjs()
+  const diffDays = now.diff(d, 'days')
+  let time
+  if (diffDays < 1) {
+    time = d.fromNow()
+  } else {
+    time = d.format('DD/MM/YYYY hh:mm A')
+  }
+
   return (
     <time
       dateTime={datetime}
       className="text-xs text-slate-500 flex items-center"
     >
-      {datetime}
+      {time}
     </time>
   )
 }
