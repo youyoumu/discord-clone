ca5711bf32bdce8bd87b10695ee3278c58cadc7a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-20 19:03:11 +0700

    Scrollable discover server

--- nextjs/app/app/servers/discover/page.tsx
+++ nextjs/app/app/servers/discover/page.tsx
@@ -1,5 +1,6 @@
 import { fetchServerDiscover } from '@/lib/actions'
 import { ServerCard } from '@/components/server-card'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 interface Server {
   id: number
@@ -16,5 +17,9 @@ export default async function Page() {
       serverId={server.id.toString()}
     />
   ))
-  return <div className="flex flex-wrap p-4 gap-4 w-full h-min">{Servers}</div>
+  return (
+    <ScrollArea>
+      <div className="flex flex-wrap p-4 gap-4 w-full h-min">{Servers}</div>
+    </ScrollArea>
+  )
 }
