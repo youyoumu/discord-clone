67c71ecf2c9999d1515684c7b0199275e8c2632f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-22 12:27:29 +0700

    Display display_name in chat

--- nextjs/components/message.tsx
+++ nextjs/components/message.tsx
@@ -8,6 +8,7 @@ export function Message({
     user: {
       username: string
       avatar_url: string
+      display_name: string
     }
     data: {
       id: string
@@ -16,12 +17,15 @@ export function Message({
     }
   }
 }) {
+  const name = message.user.display_name
+    ? message.user.display_name
+    : message.user.username
   return (
     <div className="flex gap-2 py-2">
       <Avatar url={message.user.avatar_url} />
       <div>
         <div className="flex gap-2 items-center">
-          <div className="font-medium">{message.user.username}</div>
+          <div className="font-medium">{name}</div>
           <Datetime datetime={message.data.created_at} />
         </div>
         <p className="text-sm">{message.data.content}</p>
