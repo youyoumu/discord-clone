68eaa671c2c8253b9756aeddedb31c6ad9d733c0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-24 12:10:34 +0700

    Add more tooltip to server nav

--- nextjs/components/new-server-dialog.tsx
+++ nextjs/components/new-server-dialog.tsx
@@ -6,22 +6,39 @@ import {
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+
 import { NewServerForm } from '@/components/new-server-form'
 import Image from 'next/image'
 
 export function NewServerDialog() {
   return (
-    <Dialog>
-      <DialogTrigger className="flex justify-center items-center border-none rounded-full">
-        <Image src="/plus.svg" alt="" width={60} height={60}></Image>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Create new server</DialogTitle>
-          <DialogDescription></DialogDescription>
-        </DialogHeader>
-        <NewServerForm />
-      </DialogContent>
-    </Dialog>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Dialog>
+            <DialogTrigger className="flex justify-center items-center border-none rounded-full">
+              <Image src="/plus.svg" alt="" width={60} height={60}></Image>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Create new server</DialogTitle>
+                <DialogDescription></DialogDescription>
+              </DialogHeader>
+              <NewServerForm />
+            </DialogContent>
+          </Dialog>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Create New Server</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
--- nextjs/components/server-discover-link.tsx
+++ nextjs/components/server-discover-link.tsx
@@ -5,23 +5,39 @@ import clsx from 'clsx'
 import { usePathname } from 'next/navigation'
 import Image from 'next/image'
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+
 export function ServerDiscoverLink() {
   const pathname = usePathname()
   const link = `/app/servers/discover`
 
   return (
-    <Link
-      className="flex justify-center items-center"
-      // className={
-      //   //   clsx('border border-border rounded-md p-2 transition-colors', {
-      //   //   'bg-primary text-primary-foreground hover:bg-primary/90':
-      //   //     pathname === link,
-      //   //   'hover:bg-primary/5': pathname !== link
-      //   // })
-      // }
-      href={link}
-    >
-      <Image src="/compass.svg" alt="" width={60} height={60}></Image>
-    </Link>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Link
+            className="flex justify-center items-center"
+            // className={
+            //   //   clsx('border border-border rounded-md p-2 transition-colors', {
+            //   //   'bg-primary text-primary-foreground hover:bg-primary/90':
+            //   //     pathname === link,
+            //   //   'hover:bg-primary/5': pathname !== link
+            //   // })
+            // }
+            href={link}
+          >
+            <Image src="/compass.svg" alt="" width={60} height={60}></Image>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Discover Server</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
