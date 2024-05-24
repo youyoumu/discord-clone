b21e87cb97b54e2dd0f442c27d8b0879d93d6a7c
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-24 16:29:04 +0700

    Fix avatar sizing

--- nextjs/components/avatar.tsx
+++ nextjs/components/avatar.tsx
@@ -22,7 +22,7 @@ export function Avatar({ url }: { url: string }) {
       onError={() => {
         setUrlValid(false)
       }}
-      className="object-contain rounded-full"
+      className="object-cover rounded-full w-11 h-11"
     ></Image>
   )
 }
