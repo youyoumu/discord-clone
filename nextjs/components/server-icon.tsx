9458c15eb01b4ba1bcaa1d2e73cc492d9fb8749b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-24 15:30:25 +0700

    Use regex to prevent fetching invalid img url

--- nextjs/components/server-icon.tsx
+++ nextjs/components/server-icon.tsx
@@ -12,14 +12,20 @@ import {
 
 interface Server {
   name: string
-  icon_url?: string
+  icon_url: string
 }
 export function ServerIcon({ server }: { server: Server }) {
   const name = server.name ? server.name[0].toUpperCase() : ''
-  const [urlValid, setUrlValid] = useState(true)
+  const [urlValid, setUrlValid] = useState(validateUrl(server.icon_url))
+
+  function validateUrl(value: string) {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+      value
+    )
+  }
 
   useEffect(() => {
-    setUrlValid(true)
+    setUrlValid(validateUrl(server.icon_url))
   }, [server.icon_url])
 
   if (urlValid) {
@@ -28,7 +34,7 @@ export function ServerIcon({ server }: { server: Server }) {
         <Tooltip>
           <TooltipTrigger>
             <Image
-              src={server.icon_url ? server.icon_url : 'null'}
+              src={server.icon_url ? server.icon_url : ''}
               alt=""
               width={56}
               height={56}
