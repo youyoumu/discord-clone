8f60504e715429b4b5e7923d4d514cf47896e314
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-25 23:48:00 +0700

    Add skeleton

---
+++ nextjs/app/app/servers/[serverId]/channels/[channelId]/loading.tsx
@@ -0,0 +1,72 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex flex-col p-4 gap-4 max-h-screen overflow-hidden">
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ nextjs/app/app/servers/discover/loading.tsx
@@ -0,0 +1,82 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex flex-wrap p-4 gap-4 max-h-screen overflow-hidden justify-start">
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-full min-h-64 w-64 rounded-md" />
+      </div>
+    </div>
+  )
+}
