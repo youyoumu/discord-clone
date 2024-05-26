ab3005f820a602c7b2f2ad064e5b43a8b0eb7641
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-27 00:05:05 +0700

    Install dark mode

--- nextjs/components/channel-header.tsx
+++ nextjs/components/channel-header.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
+import { ModeToggle } from './mode-toogle'
 
 export function ChannelHeader({ channelName }: { channelName: string }) {
   return (
     <div className="flex gap-1 p-2 border-b border-border font-semibold">
       <Image src="/hashtag.svg" alt="" width={24} height={24}></Image>
       {channelName}
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </div>
   )
 }
