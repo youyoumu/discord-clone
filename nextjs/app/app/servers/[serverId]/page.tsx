6e66e20e89d0b980f708c1444f59377f0bfc52f6
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-27 20:58:23 +0700

    Add vtuber icon :3

--- nextjs/app/app/servers/[serverId]/page.tsx
+++ nextjs/app/app/servers/[serverId]/page.tsx
@@ -1,3 +1,24 @@
+import Image from 'next/image'
+
 export default function Page() {
-  return <div></div>
+  return (
+    <div className="w-screen h-screen dark:bg-teal-900/20 flex items-center justify-center flex-col overflow-hidden">
+      <Image
+        src="https://raw.githubusercontent.com/youyoumu/discord-clone/main/nextjs/public/rubyonrails.png"
+        alt=""
+        width={0}
+        height={0}
+        unoptimized={true}
+        className="h-72 w-auto object-cover"
+      ></Image>
+      <Image
+        src="https://raw.githubusercontent.com/youyoumu/discord-clone/main/nextjs/public/React.png"
+        alt=""
+        width={0}
+        height={0}
+        unoptimized={true}
+        className="h-48 w-auto object-cover"
+      ></Image>
+    </div>
+  )
 }
