87d11784832e9b14214c83472d1fcff99483ef32
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-28 19:45:34 +0700

    Fix server page sizing

--- nextjs/app/app/servers/[serverId]/page.tsx
+++ nextjs/app/app/servers/[serverId]/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Page() {
   return (
-    <div className="w-screen h-screen dark:bg-teal-900/20 flex items-center justify-center flex-col overflow-hidden">
+    <div className="w-full h-full dark:bg-teal-900/20 flex items-center justify-center flex-col overflow-hidden">
       <Image
         src="https://raw.githubusercontent.com/youyoumu/discord-clone/main/nextjs/public/rubyonrails.png"
         alt=""
