17ec5c4ffe51dd2811cb3fc0b6b22a7b09200bb2
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-02 01:31:46 +0700

    Set app max height

--- nextjs/app/app/layout.tsx
+++ nextjs/app/app/layout.tsx
@@ -2,7 +2,7 @@ import { ServerNav } from '@/components/server-nav'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-screen h-screen flex max-h-[1440px]">
       <ServerNav />
       {children}
     </div>
